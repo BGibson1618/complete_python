fdebcf3885113092dfb741cffaeecc190f00186d
Brent Gibson <bgibson1618@gmail.com>
2026-02-23 17:47:20 -0600

    Redefine toml and uv.lock

---
+++ Deco.py
@@ -0,0 +1,13 @@
+"""
+Decorator Template
+"""
+
+def my_deco(fn):
+    def my_wrapper(*args, **kwargs):
+        fn(*args, **kwargs)
+    return my_wrapper
+
+def my_func(li):
+    return li * 2
+
+
